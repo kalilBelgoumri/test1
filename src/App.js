4a7777ecadcc8d66c6881aee01ddf443b33ed285
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-08-26 21:42:43 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <>
+      <Home />
         <h1>test</h1>
       </>
     </div>
