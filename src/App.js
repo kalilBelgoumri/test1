ffdc210de7108e1faaf9df0ef38f85a1e631ab15
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-08-26 21:44:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <>
-      <Home />
-        <h1>test</h1>
+        <Navbar />
+        <Home />
       </>
     </div>
   );
